de79b17bab0d2302fd0d22c23564d94a3594ba67
Derme <derme@outlook.com>
2019-08-16 21:07:48 +1000

    Add Transform Distance Script

---
+++ ArucoTrackingExample/Assets/Scripts/DistanceBetweenTransforms.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DistanceBetweenTransforms : MonoBehaviour
+{
+
+    public Transform GameObject1Transform;
+    public Transform GameObject2Transform;
+
+    public float Distance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Distance = Vector3.Distance(GameObject1Transform.position, GameObject2Transform.position);
+    }
+}
